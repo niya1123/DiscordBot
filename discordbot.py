90d34cb6570335e2d89b5a9f545e4d1b2ba51562
niya1123 <yoheikurioka@gmail.com>
2018-08-10 18:43:28 +0900

    現在時刻を答えれるように.

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 import discord # インストールした discord.py
-import passToken
+import passToken, sys
+from datetime import datetime
 
 client = discord.Client() # 接続に使用するオブジェクト
 
@@ -8,13 +9,40 @@ client = discord.Client() # 接続に使用するオブジェクト
 async def on_ready():
     print('ログインしました')
 
-# 「/neko」と発言したら「にゃーん」が返る処理
 @client.event
 async def on_message(message):
-    if message.content.startswith('/neko'):
+    
+    #!nekoでにゃーんと答える.
+    if message.content.startswith('!neko'):
         reply = 'にゃーん'
         await client.send_message(message.channel, reply)
 
+    # !timeで現在時刻を答える
+    if message.content.startswith('!time'):
+        replay = datetime.now().strftime("%Y年%m月%d日 %H時:%M分:%S秒") 
+        await client.send_message(message.channel, replay)   
+
+    # !exitでBotの終了
+    # if message.content.startswith('!exit'):
+    #     replay = "じゃあね〜！"
+    #     await client.send_message(message.channel, replay)
+    #     # sys.exit()
+    #     # client._closed.is_set(True)
+        
+    #発言チャンネル内全てのログの削除
+    # if message.content.startswith('!clean'):
+    #     clean_flag = True
+    #     while (clean_flag):
+    #         msgs = [msg async for msg in client.logs_from(message.channel)]
+    #         if len(msgs) > 1: # 1発言以下でdelete_messagesするとエラーになる
+    #             await client.delete_messages(msgs)
+    #         else:
+    #             clean_flag = False
+    #             await client.send_message(message.channel, 'ログの全削除が完了しました')    
+
+    if client.user.id in message.content: # 話しかけられたかの判定
+        reply = f'{message.author.mention} 呼んだ？' # 返信文の作成
+        await client.send_message(message.channel, reply) # 返信を送る
+
 # botの接続と起動
-# （tokenにはbotアカウントのアクセストークンを入れてください）
 client.run(passToken.token)
