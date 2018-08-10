54f6a71ac19720fcebf03cda7a72620adabf2752
niya1123 <yoheikurioka@gmail.com>
2018-08-10 17:58:23 +0900

    チュートリアル

---
+++ discordbot.py
@@ -0,0 +1,20 @@
+import discord # インストールした discord.py
+import passToken
+
+client = discord.Client() # 接続に使用するオブジェクト
+
+# 起動時に通知してくれる処理
+@client.event
+async def on_ready():
+    print('ログインしました')
+
+# 「/neko」と発言したら「にゃーん」が返る処理
+@client.event
+async def on_message(message):
+    if message.content.startswith('/neko'):
+        reply = 'にゃーん'
+        await client.send_message(message.channel, reply)
+
+# botの接続と起動
+# （tokenにはbotアカウントのアクセストークンを入れてください）
+client.run(passToken.token)
